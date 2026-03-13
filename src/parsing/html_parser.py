bc8db5ba2dea0677d85bb704597443aeabfa75d5
Justus Redlin <justusmax.redlin@detecon.com>
2026-03-13 13:54:43 +0100

    Updated Parser

--- src/parsing/html_parser.py
+++ src/parsing/html_parser.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+# this extract hrefs from a given html, only works for relative wiki URLs
 def extract_urls(html_content, base_url):
     soup = BeautifulSoup(html_content, "html.parser")
     urls = set()
@@ -11,8 +12,9 @@ def extract_urls(html_content, base_url):
         if not href:
             continue
 
-        if href.startswith("/wiki/"):
-            absolute_url = urljoin(base_url, href)
-            urls.add(absolute_url)
+        if not href.startswith("/wiki/"): # this is specific to the wiki crawler for proof wiki, not relevant for other pages
+            continue
+        absolute_url = urljoin(base_url, href)
+        urls.add(absolute_url)
 
     return urls
